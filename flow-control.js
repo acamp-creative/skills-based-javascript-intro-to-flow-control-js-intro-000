ac43cd1287495779b92c6a79d1f9fa7611a6e5db
Anaella Campana <anaellacampanaa@gmail.com>
2020-07-06 00:25:30 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,24 @@
-function basicTeenager(age) {
+function basicTeenager(age) { if(age>=13 && age<=19)
+   { return "You are a teenager!" } }
 
-}
+function teenager(age) { if(age>=13 && age<=19)
+  { return "You are a teenager!" }
+  else { return "You are not a teenager!"}}
 
-function teenager(age) {
+function ageChecker(age) { if(age>=13 && age<=19)
+   { return "You are a teenager!" }
+else if (age<=12) { return "You are a kid"}
+if (age>=19) { return "You are a grownup"}}
 
-}
+function ternaryTeenager(age) {return (age>=13 && age<=19) ? "You are a teenager" : "You are not a teenager" ; }
 
-function ageChecker(age) {
-
-}
-
-function ternaryTeenager(age) {
-
-}
-
-function switchAge(age) {
-
+function switchAge(age) {switch (expression)
+  case {age>=13 && <=19} :
+  return "You are a teenager"
+   break;
+  case {age<=12}:
+  return ("You have an age")
+  break;
+  default:
+  console.log ("You have an age")
 }
